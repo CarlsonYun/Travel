595e720e648edadd56c7e5df73e0f0076538cdd9
haungyun <577385908@qq.com>
2020-03-11 16:00:50 +0800

    第二次提交

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="home">
+      <home-header></home-header>
+    </div>
+    <!--<router-link to="/list" class="home">列表页</router-link>-->
+  </div>
+</template>
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style>
+</style>
